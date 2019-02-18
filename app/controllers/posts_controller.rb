113b2158a06c01a0473d70009c643e0ce2ab1cbe
Joey Cho <cjy12182000@naver.com>
2019-02-18 11:56:50 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-  #  @post.new(post_params)
+      @post.new(post_params)
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
     end
-
+    
   end
 
   private
