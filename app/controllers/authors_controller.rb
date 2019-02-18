638052c1c72c633d7b6b03aa3ea052e9e4a4c58a
Joey Cho <cjy12182000@naver.com>
2019-02-18 11:34:12 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,18 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create!(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+     # If--and only if--the post is valid, do what we usually do.
+     @author.save
+     redirect_to author_path(@author)
+   else
+     # If the post is invalid, hold on to @post, because it is now full of
+     # useful error messages, and re-render the :new page, which knows how
+     # to display them alongside the user's entries.
+     render :new
+   end
+    
+    
   end
 
   private
